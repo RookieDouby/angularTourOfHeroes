2f49324ffc617f4960c17a18ba574761e77eb7f7
张红豆 <zhanghongdaou@encdata.cn>
2019-12-12 20:09:36 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
+
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'heroes', component: HeroesComponent},
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,34 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {
+    id: 11, name: '后裔'
+  },
+  {
+    id: 12, name: '孙尚香'
+  },
+  {
+    id: 13, name: '成吉思汗'
+  },
+  {
+    id: 14, name: '百里守约'
+  },
+  {
+    id: 15, name: '伽罗'
+  },
+  {
+    id: 16, name: '马可波罗'
+  },
+  {
+    id: 17, name: '虞姬'
+  },
+  {
+    id: 18, name: '鲁班七号'
+  },
+  {
+    id: 19, name: '李元芳'
+  },
+  {
+    id: 20, name: '黄忠'
+  },
+]
